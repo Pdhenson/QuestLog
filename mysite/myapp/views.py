7c51ffae1df8ca4c0e207878c27d851219e239d6
Pdhenson <Pdhenson@mail.csuchico.edu>
2019-02-18 12:30:50 -0800

    Assignment 3 Submission

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("CINS465 Hello World")
+    context = {
+        "body":"CINS465 Hello World!",
+        "title":"Assignment 3 Hello World!",
+        "next":"Next",
+    }
+    return render(request, "base.html", context=context)
